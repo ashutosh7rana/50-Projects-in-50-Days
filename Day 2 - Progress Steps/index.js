07341e2a44f0205a41fc8d45b584ccf8ffca1cda
Ashutosh Rana <ranaashutosh0560@gmail.com>
2025-07-27 00:02:38 +0530

    Day 3

---
+++ Day 2 - Progress Steps/index.js
@@ -0,0 +1,54 @@
+const progress = document.querySelector('.progress');
+const progressSteps = document.querySelectorAll('.steps');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+const container = document.querySelector('.container');
+
+let currentActive = 0;
+
+next.addEventListener('click', () => {
+    currentActive++;
+    if (currentActive > progressSteps.length) {
+        currentActive = progressSteps.length;
+    }
+    update('next');
+});
+
+prev.addEventListener('click', () => {
+    currentActive--;
+    if (currentActive < 1) {
+        currentActive = 1;
+    }
+    update('prev');
+});
+
+function update(button) {
+    if (button === 'next') {
+        container.classList.remove('step-' + (currentActive - 1));
+    } else if (button === 'prev') {
+        container.classList.remove('step-' + (currentActive + 1));
+    }
+    container.classList.add('step-' + (currentActive));
+    //progress.style.width = ((currentActive - 1) / (progressSteps.length - 1)) * 100 + '%';
+    progressSteps.forEach((step, index) => {        
+        if (index < currentActive) {
+            step.classList.add('active');            
+        } else {
+            step.classList.remove('active');
+        }
+    });
+
+    if (currentActive === 1) {
+        prev.classList.add('disabled');
+    } else {
+        prev.classList.remove('disabled');
+    }
+
+    if (currentActive === progressSteps.length) {
+        next.classList.remove('active');
+        next.classList.add('disabled');
+    } else {
+        next.classList.remove('disabled');
+        next.classList.add('active');
+    }
+}
